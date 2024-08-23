eb36ccebfeebdc901be20256fe120db036a78cd7
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-23 09:03:38 -0700

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
 
+const App = ()=>{
+
+
+
+
+  return (
+    <div>
+    <form>
+      <input type="text" />
+      <button>Click</button>
+    </form>
+    </div>
+  )
+
+  
+}
+
+export default App;
