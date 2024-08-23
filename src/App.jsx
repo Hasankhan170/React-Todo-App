258b71841d4b1fb4170b0ab6892fa06f4ab777fe
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-23 09:17:42 -0700

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 
+import { useRef  } from "react";
+
 const App = ()=>{
+
+
+  // const [arr,setArr] = useState([])
+  const todoValue = useRef()
+
+
+  const formValue = (e)=>{
+    e.preventDefault();
+    console.log(todoValue.current.value);
+ 
+  
+
+
+    todoValue.current.value = ""
+    
+    
+  }
 
 
 
 
   return (
     <div>
-    <form>
-      <input type="text" />
-      <button>Click</button>
+    <form onSubmit={formValue}>
+      <input type="text" placeholder="Enter Todo" ref={todoValue}/>
+      <button type="submit">Click</button>
     </form>
     </div>
   )
 
-  
+
 }
 
 export default App;
