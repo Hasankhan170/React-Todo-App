a279155e9e6bb6f83e13cdffd6c26158980e388c
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-24 01:00:06 -0700

    prompt validation

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,10 @@ function App(){
 
   const editTodo =(index)=>{
     const newValue = prompt('Enter New Value')
+    if(newValue === null ||newValue.trim() === ''){
+      alert('No change')
+      return;
+    }
     arr.splice(index,1,newValue)
     setArr([...arr])
   }
@@ -34,22 +38,22 @@ function App(){
     <div className="text-center mt-3">
       <h1>Todo App</h1>
       <form className="text-center p-5  d-flex justify-content-center flex-wrap" onSubmit={formValue}>
-        <input className="px-4 mb-2 py-2 form-control border-success rounded" type="text" placeholder="Enter Todo" ref={inputValue} />
+        <input required className="px-4 mb-2 py-2 form-control border-success rounded" type="text" placeholder="Enter Todo" ref={inputValue} />
         <button className="btn btn-primary form-control px- mb-2 py-2 border " type="submit">Click</button>
       </form>
       <div>
         {
           arr.map((item,index)=>{
-            return <div className="m-2 d-flex justify-content-around align-items-center" key={index}>
+            return <div className="m-2 p-2 bg-light flex-wrap d-flex justify-content-around align-items-center" key={index}>
               <h4 className="px-2">{item}</h4>
-        <div>
-              <button onClick={()=> deleteTodo(index)}>Delete Todo</button>
-              <button onClick={()=> editTodo(index)}>Edit Todo</button>
+          <div className="d-flex">
+              <button className="btn btn-danger form-control px-3 border " onClick={()=> deleteTodo(index)}><i className="fa-solid fa-delete-left"></i></button>
+              <button className="btn btn-success form-control px-3 border " onClick={()=> editTodo(index)}><i className="fa-regular fa-pen-to-square"></i></button>
+          </div>
         </div>
-            </div>
           })
         }
-        </div>
+      </div>
     </div>
   )
 }
