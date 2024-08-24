fb423e12f0b564e8ccc5c5f7a87a8dc66c9a814b
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-24 00:41:14 -0700

    "Updated input and button styles, and rearranged todo list item layout"

--- src/App.jsx
+++ src/App.jsx
@@ -34,16 +34,18 @@ function App(){
     <div className="text-center mt-3">
       <h1>Todo App</h1>
       <form className="text-center p-5  d-flex justify-content-center flex-wrap" onSubmit={formValue}>
-        <input className="px-4 mb-2 py-2 form-control border-success" type="text" placeholder="Enter Todo" ref={inputValue} />
-        <button className="btn btn-primary px-4 mb-2 py-2 border mx-2 border-0" type="submit">Click</button>
+        <input className="px-4 mb-2 py-2 form-control border-success rounded" type="text" placeholder="Enter Todo" ref={inputValue} />
+        <button className="btn btn-primary form-control px- mb-2 py-2 border " type="submit">Click</button>
       </form>
       <div>
         {
           arr.map((item,index)=>{
-            return <div key={index}>
-              <h6>{item}</h6>
+            return <div className="m-2 d-flex justify-content-around align-items-center" key={index}>
+              <h4 className="px-2">{item}</h4>
+        <div>
               <button onClick={()=> deleteTodo(index)}>Delete Todo</button>
               <button onClick={()=> editTodo(index)}>Edit Todo</button>
+        </div>
             </div>
           })
         }
