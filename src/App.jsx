4e66aab2ea5f32915e4fc497de7ba1ac444f1dcb
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-24 00:23:35 -0700

    style

--- src/App.jsx
+++ src/App.jsx
@@ -31,23 +31,23 @@ function App(){
   }
 
   return (
-    <div className="text-center">
+    <div className="text-center mt-3">
       <h1>Todo App</h1>
-      <form onSubmit={formValue}>
-        <input type="text" placeholder="Enter Todo" ref={inputValue} />
-        <button type="submit" className="">Click</button>
+      <form className="text-center p-5  d-flex justify-content-center flex-wrap" onSubmit={formValue}>
+        <input className="px-4 mb-2 py-2 form-control border-success" type="text" placeholder="Enter Todo" ref={inputValue} />
+        <button className="btn btn-primary px-4 mb-2 py-2 border mx-2 border-0" type="submit">Click</button>
       </form>
-      <ul>
+      <div>
         {
           arr.map((item,index)=>{
             return <div key={index}>
-              <li>{item}</li>
-              <button onClick={ ()=> deleteTodo(index)}>Delete Todo</button>
-              <button onClick={ ()=> editTodo(index)}>Edit Todo</button>
+              <h6>{item}</h6>
+              <button onClick={()=> deleteTodo(index)}>Delete Todo</button>
+              <button onClick={()=> editTodo(index)}>Edit Todo</button>
             </div>
           })
         }
-        </ul>
+        </div>
     </div>
   )
 }
